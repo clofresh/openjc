584cf2c0287d3d71553b2babbdecccb38a7106aa
Carlo Cabanilla <carlo@datadoghq.com>
2013-12-04 00:49:04 -0500

    fix where the templates are

--- ckanext-openjc/ckanext/openjc/plugin.py
+++ ckanext-openjc/ckanext/openjc/plugin.py
@@ -3,4 +3,4 @@ import ckan.plugins as plugins
 
 class OpenJcPlugin(plugins.SingletonPlugin):
 	def update_config(self, config):
-		config['extra_template_paths'] = 'ckanext-openjc/templates'
+		plugins.toolkit.add_template_directory(config, 'templates')
